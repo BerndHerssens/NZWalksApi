66dea7576e09018bcb1fbfef446417c52d44fd42
bernd_herssens <berndherssens@hotmail.com>
2024-05-13 10:57:38 +0200

    Add project files.

---
+++ NZWalksApi/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+       public string GetName()
+        {
+            return "Bernd";
+        }
+    }
+}
---
+++ NZWalksApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetASpecificWeather")]
+        public WeatherForecast GetSpecificWeather()
+        {
+            WeatherForecast forecast = new WeatherForecast();
+            forecast.Date = DateOnly.FromDateTime(DateTime.Now);
+            forecast.TemperatureC = 22;
+            forecast.Summary = "Het is zo warm, dat ik vijvers laat vloeien uit mijn oksels.";
+            return forecast;
+        }
+
+        [HttpGet]
+        [Route("GetASpecificWeatherWithID/{id:int}")]
+        public WeatherForecast GetSpecificWeather(int id)
+        {
+            WeatherForecast forecast = new WeatherForecast();
+            forecast.Date = DateOnly.FromDateTime(DateTime.Now);
+            forecast.TemperatureC = 22;
+            forecast.Summary = "Het is zo warm, dat ik vijvers laat vloeien uit mijn oksels.";
+            return forecast;
+        }
+    }
+}
